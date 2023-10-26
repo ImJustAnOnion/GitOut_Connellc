5f59ce2ec1b0c1dce336ef27c151b07aa8841539
Lai Connelly <laicoal@hotmail.com>
2023-10-26 18:36:31 -0400

    Add project files.

---
+++ GitOut_Connellc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitOut_Connellc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
